cc5d514e71a0e77a4845c86abe3b15e1f67baff0
abdulsamad2929 <asramzani@gmail.com>
2022-12-28 18:35:32 +0500

    optimize work flow action for CEO

--- frappe/workflow/doctype/workflow_action/workflow_action_list.js
+++ frappe/workflow/doctype/workflow_action/workflow_action_list.js
@@ -1,4 +1,13 @@
 frappe.listview_settings['Workflow Action'] = {
+	onload: function(me) {
+		if (Object.values(frappe.route_options).length == 0){
+			frappe.route_options = {
+				"reference_doctype":["In",['Payment Order,Employee,Purchase Order,Expense Entry,Customer,Supplier,Item Daily Rate,Payment Entry,Journal Entry,Payment Advice,BOM']],
+				"user": frappe.session.user_email,
+				"creation":["Between",[frappe.datetime.add_days(frappe.datetime.get_today(), -31),frappe.datetime.get_today()]]
+			};
+		}
+	},
 	get_form_link: (doc) => {
 		let doctype = '';
 		let docname = '';

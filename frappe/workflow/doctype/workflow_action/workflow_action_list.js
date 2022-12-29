b086f24fcd833e9c76c34aa20e456d15f036a61b
abdulsamad2929 <asramzani@gmail.com>
2022-12-29 15:43:15 +0500

    change in WFA

--- frappe/workflow/doctype/workflow_action/workflow_action_list.js
+++ frappe/workflow/doctype/workflow_action/workflow_action_list.js
@@ -3,8 +3,9 @@ frappe.listview_settings['Workflow Action'] = {
 		if (Object.values(frappe.route_options).length == 0){
 			frappe.route_options = {
 				"reference_doctype":["In",['Payment Order,Employee,Purchase Order,Expense Entry,Customer,Supplier,Item Daily Rate,Payment Entry,Journal Entry,Payment Advice,BOM']],
-				//"user": frappe.session.user_email,
-				"user": 'zubair@gourmetpakistan.com',
+				"user": frappe.session.user_email,
+				//"user": 'zubair@gourmetpakistan.com',
+				"status": "Open",
 				"creation":["Between",[frappe.datetime.add_days(frappe.datetime.get_today(), -31),frappe.datetime.get_today()]]
 			};
 		}

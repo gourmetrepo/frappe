182784c551af605acbb602ebd25422d52825d860
abdulsamad2929 <asramzani@gmail.com>
2023-01-10 15:59:34 +0500

    Change in workflow action filter

--- frappe/workflow/doctype/workflow_action/workflow_action_list.js
+++ frappe/workflow/doctype/workflow_action/workflow_action_list.js
@@ -2,11 +2,11 @@ frappe.listview_settings['Workflow Action'] = {
 	onload: function(me) {
 		if (Object.values(frappe.route_options).length == 0){
 			frappe.route_options = {
-				"reference_doctype":["In",['Payment Order,Employee,Purchase Order,Expense Entry,Customer,Supplier,Item Daily Rate,Payment Entry,Journal Entry,Payment Advice,BOM']],
+				"reference_doctype":["in",['Payment Order,Employee,Purchase Order,Expense Entry,Customer,Supplier,Item Daily Rate,Payment Entry,Journal Entry,Payment Advice,BOM']],
 				"user": frappe.session.user_email,
 				//"user": 'zubair@gourmetpakistan.com',
 				"status": "Open",
-				"creation":["Between",[frappe.datetime.add_days(frappe.datetime.get_today(), -31),frappe.datetime.get_today()]]
+				"creation":["Between",[frappe.datetime.add_days(frappe.datetime.get_today(), -10),frappe.datetime.get_today()]]
 			};
 		}
 	},

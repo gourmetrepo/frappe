a6d649f64e8899971c2929ae0060d4d001656f2c
IRFAN BAJWA <98512717+irfanbajwase@users.noreply.github.com>
2023-01-17 21:26:31 +0500

    workflow action board

--- frappe/workflow/doctype/workflow_action/workflow_action_list.js
+++ frappe/workflow/doctype/workflow_action/workflow_action_list.js
@@ -8,6 +8,27 @@ frappe.listview_settings['Workflow Action'] = {
 				"status": "Open",
 				"creation":["Between",[frappe.datetime.add_days(frappe.datetime.get_today(), -10),frappe.datetime.get_today()]]
 			};
+
+			frappe.call({
+				method: 'nrp_manufacturing.apis.purchase_order.po_total',
+				args: {
+					'doc': frappe.route_options
+				},
+				callback: function(r) {
+					if (!r.exc) {
+						if (r.message.length > 0){
+							var data =""
+						data += '<div style="padding:10px 20px;">'
+						$.each(r.message, function( index, value ) {
+							data += '<div class="inner-row"><div style="width:60px;font-weight:bold;display: inline-block;">'+value.company+' </div>' + '<span class="total"> '+value.total+'</span></div>' 
+							// console.log( index + ": " + value.company );
+						  });
+						data += '</div">'
+						$(data).insertBefore(".page-form");
+						}
+					}
+				}
+			});
 		}
 	},
 	get_form_link: (doc) => {

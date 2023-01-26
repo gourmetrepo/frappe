2e67d737ac2cdcd6db53550f1b657961f5dfddd3
IRFAN BAJWA <98512717+irfanbajwase@users.noreply.github.com>
2023-01-26 13:52:00 +0500

    JS changes workflow

--- frappe/workflow/doctype/workflow_action/workflow_action_list.js
+++ frappe/workflow/doctype/workflow_action/workflow_action_list.js
@@ -17,10 +17,9 @@ frappe.listview_settings['Workflow Action'] = {
 				callback: function(r) {
 					if (!r.exc) {
 						if (r.message.length > 0){
-							var data =""
-						
+						var data =""						
 						data += '<div class="row visible-section" style="border-bottom: 1px solid rgb(209, 216, 221);border-top: 1px solid rgb(209, 216, 221);padding: 1px 15px;  position:relative; margin-top: 5px; margin-left:0px; margin-right:0px">'	
-						data += '<p style="margin:0px;border-bottom:1px solid;"><a class="section-head collapsed h6 uppercase" style="display:block;margin:0px;background-color: transparent;text-align: left;padding: 15px;color:#000;padding-right: 0;" data-toggle="collapse" href="#multiCollapseExample1">Open Purchase Order Wise Total Amount</a><span class="octicon collapse-indicator octicon-chevron-down" style="position:absolute;right:15px;top:15px;"></span></p>'
+						data += '<p style="margin:0px;border-bottom:1px solid #d1d8dd;margin-bottom: 13px;"><a class="section-head collapsed h6 uppercase" style="display:block;margin:0px;background-color: transparent;text-align: left;padding: 15px;color:#000;padding-right: 0;" data-toggle="collapse" href="#multiCollapseExample1">Open Purchase Order Wise Total Amount</a><span class="octicon collapse-indicator octicon-chevron-down" style="position:absolute;right:15px;top:15px;"></span></p>'
 						data += '<div class="collapse" id="multiCollapseExample1">'
 						data += '<div class="card card-body" >'
 

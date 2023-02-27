653813911209200d2447d78b37e70594c38db4b7
shoaibrehmat88 <86551638+shoaibrehmat88@users.noreply.github.com>
2023-02-27 18:04:35 +0500

    change in doc

--- frappe/model/naming.py
+++ frappe/model/naming.py
@@ -77,7 +77,9 @@ def set_name_from_naming_options(autoname, doc):
 		doc.name = make_autoname(autoname, doc=doc)
 	else:
 		import time
-		doc.name = str(time.time_ns())
+		# doc.name = str(time.time_ns())
+		_t = str(time.time())
+		doc.name = _t.replace(".","")
 
 def set_name_by_naming_series(doc):
 	"""Sets name by the `naming_series` property"""
@@ -110,7 +112,9 @@ def make_autoname(key="", doctype="", doc=""):
 	if key == "hash":
 		# return frappe.generate_hash(doctype, 10)
 		import time
-		return str(time.time_ns())
+		# return str(time.time_ns())
+		_t = str(time.time())
+		return _t.replace(".","")
 
 	if "#" not in key:
 		key = key + ".#####"

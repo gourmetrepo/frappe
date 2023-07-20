810c20b5998ba79f46f7c2990fa0e112b995ae3e
abdulsamad2929 <asramzani@gmail.com>
2023-07-20 19:31:43 +0500

    Change query

--- frappe/core/doctype/role_permission_for_page_and_report/role_permission_for_page_and_report.py
+++ frappe/core/doctype/role_permission_for_page_and_report/role_permission_for_page_and_report.py
@@ -96,7 +96,7 @@ class RolePermissionforPageandReport(Document):
 
 @frappe.whitelist()
 def remove_cache(doctype,role):
-	role_users = frappe.db.sql("SELECT DISTINCT parent FROM `tabHas Role` WHERE ROLE=%s", (role))
+	role_users = frappe.db.sql("SELECT DISTINCT parent FROM `tabHas Role` WHERE parenttype='user' AND ROLE=%s", (role))
 	module_name = frappe.db.sql("SELECT module FROM `tabReport` WHERE NAME=%s", (doctype))
 	for u in role_users:
 		frappe.cache().delete_value(frappe.scrub(u.parent)+'_module_'+frappe.scrub(module_name.module))

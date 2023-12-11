33564fbdd4cb809f60122c71820e8346bd074e8e
bakar.grm <101788077+bakar-grm@users.noreply.github.com>
2023-12-11 18:46:53 +0500

    Update list_view.js
    user who will see the count of documents

--- frappe/public/js/frappe/list/list_view.js
+++ frappe/public/js/frappe/list/list_view.js
@@ -319,6 +319,7 @@ frappe.views.ListView = class ListView extends frappe.views.BaseList {
 	freeze() {
 		if (this.list_view_settings && !this.list_view_settings.disable_count) {
 			this.$result.find('.list-count').html(`<span>${__('Refreshing')}...</span>`);
+			//this.$result.find('.list-count').html(``);
 		}
 	}
 
@@ -420,7 +421,13 @@ frappe.views.ListView = class ListView extends frappe.views.BaseList {
 	}
 
 	render_count() {
-		if (!this.list_view_settings.disable_count) {
+		if ( (frappe.session.user_email=='muhammadyasir@gourmetpakistan.com' || frappe.session.user_email=='muhammad.rauf@gourmetpakistan.com' || frappe.session.user_email=='anwar.haq@gourmetpakistan.com' || frappe.session.user_email=='khizer.shujra@gourmetpakistan.com' || frappe.session.user_email=='rizwan.ali@gourmetpakistan.com'  || frappe.session.user_email=='zubair@gourmetpakistan.com' || frappe.session.user_email=='zulqarnain@gourmetpakistan.com' || frappe.session.user_email=='shaharyar@gourmetpakistan.com')) {
+			if(this.doctype!='Stock Ledger Entry' && this.doctype!='GL Entry' && this.doctype!='Batch'){
+			this.get_count_str().then(str => {
+				this.$result.find('.list-count').html(`<span>${str}</span>`);
+			});
+		}}
+		else if(!this.list_view_settings.disable_count){
 			this.get_count_str().then(str => {
 				this.$result.find('.list-count').html(`<span>${str}</span>`);
 			});

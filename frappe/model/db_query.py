79563a61ff9fb7ad9ecd0cb96996e5ce4a1b11a3
abdulsamad2929 <asramzani@gmail.com>
2024-04-01 15:13:52 +0500

    Change in If null

--- frappe/model/db_query.py
+++ frappe/model/db_query.py
@@ -629,13 +629,14 @@ class DatabaseQuery(object):
 				if frappe.get_system_settings("apply_strict_user_permissions"):
 					condition = ""
 				else:
-					#samad ifnull 
-					#col_list_set = ['company','name','posting_time','posting_date','transaction_date','name', 'creation', 'modified', 'modified_by', 'owner', 'docstatus', 'parent','parentfield', 'parenttype', 'idx','_user_tags']
-					#if(df.get('fieldname') not in col_list_set):
+					#samad ifnull
+					empty_value_condition=''
+					col_list_set = ['company','name','posting_time','posting_date','transaction_date','name', 'creation', 'modified', 'modified_by', 'owner', 'docstatus', 'parent','parentfield', 'parenttype', 'idx','_user_tags']
+					if(df.get('fieldname') not in col_list_set):
 						empty_value_condition = "ifnull(`tab{doctype}`.`{fieldname}`, '')=''".format(
 							doctype=self.doctype, fieldname=df.get('fieldname')
 						)
-						condition = empty_value_condition + " or "
+					condition = empty_value_condition + " or "
 
 				for permission in user_permission_values:
 					if not permission.get('applicable_for'):

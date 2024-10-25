4bbb9da1249d2dca8f256e13a86a6ff920e84347
Shoaib Rehmat <86551638+shoaibrehmat88@users.noreply.github.com>
2024-10-25 09:51:36 +0500

    changes for read from replica

--- frappe/core/doctype/prepared_report/prepared_report.py
+++ frappe/core/doctype/prepared_report/prepared_report.py
@@ -27,8 +27,9 @@ class PreparedReport(Document):
 
 
 def run_background(prepared_report):
-	# from frappe import connect_replica
-	# connect_replica()
+	if frappe.conf.read_from_replica == 1:
+		from frappe import connect_replica
+		connect_replica()
 
 	instance = frappe.get_doc("Prepared Report", prepared_report)
 	report = frappe.get_doc("Report", instance.ref_report_doctype)
@@ -57,17 +58,17 @@ def run_background(prepared_report):
 		instance.report_end_time = frappe.utils.now()
 		instance.save(ignore_permissions=True)
 		frappe.db.commit()
-		# from frappe import connect_live
-		# connect_live()
-		# instance = frappe.get_doc("Prepared Report", prepared_report)
-		# instance.status = "Completed"
-		# instance.columns = json.dumps(result["columns"])
-		# instance.report_end_time = frappe.utils.now()
-		# instance.save(ignore_permissions=True)
-		# _file = frappe.get_doc(file_doc)
-		# _file.save(ignore_permissions=True)
-
-		# frappe.db.commit()
+		if frappe.conf.read_from_replica == 1:
+			from frappe import connect_live
+			connect_live()
+			instance = frappe.get_doc("Prepared Report", prepared_report)
+			instance.status = "Completed"
+			instance.columns = json.dumps(result["columns"])
+			instance.report_end_time = frappe.utils.now()
+			instance.save(ignore_permissions=True)
+			_file = frappe.get_doc(file_doc)
+			_file.save(ignore_permissions=True)
+			frappe.db.commit()
 	except Exception:
 		frappe.log_error(frappe.get_traceback())
 		instance = frappe.get_doc("Prepared Report", prepared_report)
@@ -75,13 +76,14 @@ def run_background(prepared_report):
 		instance.error_message = frappe.get_traceback()
 		instance.save(ignore_permissions=True)
 		frappe.db.commit()
-		# from frappe import connect_live
-		# connect_live()
-		# instance = frappe.get_doc("Prepared Report", prepared_report)
-		# instance.status = "Error"
-		# instance.error_message = frappe.get_traceback()
-		# instance.save(ignore_permissions=True)
-		# frappe.db.commit()
+		if frappe.conf.read_from_replica == 1:
+			from frappe import connect_live
+			connect_live()
+			instance = frappe.get_doc("Prepared Report", prepared_report)
+			instance.status = "Error"
+			instance.error_message = frappe.get_traceback()
+			instance.save(ignore_permissions=True)
+			frappe.db.commit()
 
 	frappe.publish_realtime(
 		"report_generated",

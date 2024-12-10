41858dbea2cad7861307738495ca816242ce9be4
moeiz-gourmet <163851544+moeiz-gourmet@users.noreply.github.com>
2024-12-10 16:54:46 +0500

    Ingnoring Workflows on Batch Payment Request

--- frappe/workflow/doctype/workflow_action/workflow_action.py
+++ frappe/workflow/doctype/workflow_action/workflow_action.py
@@ -33,31 +33,33 @@ def has_permission(doc, user):
 		return False
 
 def process_workflow_actions(doc, state):
-	workflow = get_workflow_name(doc.get('doctype'))
-	if not workflow: return
 
-	if state == "on_trash":
-		clear_workflow_actions(doc.get('doctype'), doc.get('name'))
-		return
+	if not frappe.flags.ignore_workflow:
+		workflow = get_workflow_name(doc.get('doctype'))
+		if not workflow: return
 
-	if is_workflow_action_already_created(doc): return
+		if state == "on_trash":
+			clear_workflow_actions(doc.get('doctype'), doc.get('name'))
+			return
 
-	clear_old_workflow_actions(doc)
-	update_completed_workflow_actions(doc)
-	clear_doctype_notifications('Workflow Action')
+		if is_workflow_action_already_created(doc): return
 
-	next_possible_transitions = get_next_possible_transitions(workflow, get_doc_workflow_state(doc), doc)
+		clear_old_workflow_actions(doc)
+		update_completed_workflow_actions(doc)
+		clear_doctype_notifications('Workflow Action')
 
-	if not next_possible_transitions: return
+		next_possible_transitions = get_next_possible_transitions(workflow, get_doc_workflow_state(doc), doc)
 
-	user_data_map = get_users_next_action_data(next_possible_transitions, doc)
+		if not next_possible_transitions: return
 
-	if not user_data_map: return
+		user_data_map = get_users_next_action_data(next_possible_transitions, doc)
 
-	create_workflow_actions_for_users(user_data_map.keys(), doc)
+		if not user_data_map: return
 
-	if send_email_alert(workflow):
-		enqueue(send_workflow_action_email, queue='short', users_data=list(user_data_map.values()), doc=doc)
+		create_workflow_actions_for_users(user_data_map.keys(), doc)
+
+		if send_email_alert(workflow):
+			enqueue(send_workflow_action_email, queue='short', users_data=list(user_data_map.values()), doc=doc)
 
 @frappe.whitelist(allow_guest=True)
 def apply_action(action, doctype, docname, current_state, user=None, last_modified=None):
@@ -183,15 +185,18 @@ def get_users_next_action_data(transitions, doc):
 
 
 def create_workflow_actions_for_users(users, doc):
-	for user in users:
-		frappe.get_doc({
-			'doctype': 'Workflow Action',
-			'reference_doctype': doc.get('doctype'),
-			'reference_name': doc.get('name'),
-			'workflow_state': get_doc_workflow_state(doc),
-			'status': 'Open',
-			'user': user
-		}).insert(ignore_permissions=True)
+	if frappe.flags.ignore_workflow:
+		pass
+	else:
+		for user in users:
+			frappe.get_doc({
+				'doctype': 'Workflow Action',
+				'reference_doctype': doc.get('doctype'),
+				'reference_name': doc.get('name'),
+				'workflow_state': get_doc_workflow_state(doc),
+				'status': 'Open',
+				'user': user
+			}).insert(ignore_permissions=True)
 
 def send_workflow_action_email(users_data, doc):
 	common_args = get_common_email_args(doc)

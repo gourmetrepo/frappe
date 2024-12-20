15835df0e1013e203b28f5d9f653a197d58baa39
attiqdeveloper <attiqur.rehman@gourmetpakistan.com>
2024-12-20 14:01:50 +0500

    Update queue name in core method

--- frappe/model/document.py
+++ frappe/model/document.py
@@ -1216,7 +1216,7 @@ class Document(BaseDocument):
 			
 
 		self.lock()
-		enqueue('frappe.model.document.execute_action', doctype=self.doctype, name=self.name,queue=queue_name,
+		enqueue('frappe.model.document.execute_action', doctype=self.doctype, name=self.name,queue=queue,
 			action=action, **kwargs)
 
 	def lock(self, timeout=None):

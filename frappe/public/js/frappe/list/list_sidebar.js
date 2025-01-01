0f74c278cc724582f77b8eec8d285ff6a2d519cb
Eesa Wazir <eesa.wazir@gourmetpakistan.com>
2025-01-01 09:37:36 +0500

    Ticket 132365: remove Gantt from view options

--- frappe/public/js/frappe/list/list_sidebar.js
+++ frappe/public/js/frappe/list/list_sidebar.js
@@ -51,7 +51,7 @@ frappe.views.ListSidebar = class ListSidebar {
 
 		if (frappe.views.calendar[this.doctype]) {
 			this.sidebar.find('.list-link[data-view="Calendar"]').removeClass("hide");
-			this.sidebar.find('.list-link[data-view="Gantt"]').removeClass('hide');
+			// this.sidebar.find('.list-link[data-view="Gantt"]').removeClass('hide');
 			show_list_link = true;
 		}
 		//show link for kanban view

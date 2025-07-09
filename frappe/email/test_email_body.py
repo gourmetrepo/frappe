502614ef71e8fd1606bc532c21dfc1be3e24d8ba
moeiz-gourmet <163851544+moeiz-gourmet@users.noreply.github.com>
2025-07-09 15:57:26 +0500

    Updated css in asses

--- frappe/email/test_email_body.py
+++ frappe/email/test_email_body.py
@@ -195,8 +195,8 @@ d85b; border-radius:8px; display:inline-block; height:8px; margin-right:5px=
 Content-Type: text/plain; charset=utf-8
 Content-Disposition: inline
 Content-Transfer-Encoding: 8bit
-From: test1_@erpnext.com
-Reply-To: test2_@erpnext.com
+From: gerp@gicoh.com
+Reply-To: gerp@gicoh.com
 """ + text_content_bytes
 
 		mail = Email(content_bytes)
